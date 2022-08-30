0d5868c0f5659effaec94cf94f438b4667955a79
Zicklag <zicklag@katharostech.com>
2022-08-30 17:38:39 -0500

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import deno from "@astrojs/deno";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: deno({
+    port: 8910,
+  }),
+});
